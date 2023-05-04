007d7f9e7ea4471226505959e0f243286f1d8f17
alexisthach <thyden26@gmail.com>
2023-05-04 12:47:11 +0200

    JOptionPane remplacé par une JDialog
    c'est mieux pour pouvoir custom les boutons, fond...

--- src/View/GameFrame.java
+++ src/View/GameFrame.java
@@ -294,15 +294,7 @@ public class GameFrame extends Frame {
         forfeit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                int response = JOptionPane.showConfirmDialog(null,
-                        "Voulez-vous abandonner la partie ?", "Abandonner",
-                        JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-                if (response == JOptionPane.YES_OPTION) {
-                    ui.changePage(InterfacePage.MENU);
-                    System.out.println("Bah alors ? T'abandonnes car t'es une grosse merde.");
-                } else if (response == JOptionPane.NO_OPTION) {
-                    System.out.println("Ouf tu m'as fait peur ! Abandonner c'est pour les faibles.");
-                }
+                dialog();
             }
         });
     }
@@ -355,6 +347,57 @@ public class GameFrame extends Frame {
     }
 
     public void showWinWindow(String winnerName){
+
+    }
+    public void dialog(){
+        JDialog dialog = new JDialog();
+        dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        dialog.setTitle("Confirmation");
+        dialog.setModal(true);
+
+        // JPanel that contains the text and buttons
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        // Text
+        JLabel question = new JLabel("Êtes-vous sûr de vouloir abandonner ?");
+        question.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+        panel.add(question, BorderLayout.NORTH);
+
+        // Buttons
+        JButton yesButton = new JButton("Oui");
+        yesButton.setBackground(Color.GREEN);
+        yesButton.setForeground(Color.WHITE);
+        yesButton.setPreferredSize(new Dimension(80, 30));
+        yesButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        yesButton.addActionListener(e -> {
+            System.out.println("Tu as abandonné.");
+            ui.changePage(InterfacePage.MENU);
+            dialog.dispose(); // dialog closes after clicking on the button
+        });
+
+        JButton noButton = new JButton("Non");
+        noButton.setBackground(Color.RED);
+        noButton.setForeground(Color.WHITE);
+        noButton.setPreferredSize(new Dimension(80, 30));
+        noButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        noButton.addActionListener(e -> {
+            System.out.println("Tu continues.");
+            dialog.dispose(); // dialog closes after clicking on the button
+        });
+
+        // Add buttons to a custom panel
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(yesButton);
+        buttonPanel.add(noButton);
+        panel.add(buttonPanel, BorderLayout.CENTER);
+
+        // Display the dialog
+        dialog.add(panel);
+        dialog.pack();
+        dialog.setLocationRelativeTo(null);
+        dialog.setResizable(false);
+        dialog.setVisible(true);
 
     }
 }
