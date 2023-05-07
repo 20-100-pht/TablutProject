1a334765cae61f321f250f9e4af7b54810b01770
Sylvain Joubert <119332727+Syl21J@users.noreply.github.com>
2023-05-07 15:01:01 +0200

    Ajout de paramètres pour choisir les ia et leurs types

---
+++ src/AI/AIDifficulty.java
@@ -0,0 +1,15 @@
+package AI;
+
+public enum AIDifficulty {
+    RANDOM(0x0),
+    EASY(0x1),
+    MID(0x2),
+    HARD(0x3);
+
+    private int value;
+    AIDifficulty(int value) {this.value = value;}
+    public int getValue() {
+        return value;
+    }
+
+}
