dd5e852ab01c85a2a23d8db9b0cfda1260fc109f
20-100-pht <20-100-pht@valve.com>
2023-05-20 17:27:25 +0200

    Changes by oliver Debug IA RANDOM

--- src/AI/AIRandom.java
+++ src/AI/AIRandom.java
@@ -14,11 +14,11 @@ public class AIRandom extends AI{
 
         if(gm.getEndGameType() == ResultGame.DEFENDER_WIN){
             //If defender can win in max 3 moves (Defender - Attacker - Defender)
-            return -100000;
+            return -100000*(depth+1);
 
         }else if(gm.getEndGameType() == ResultGame.ATTACKER_WIN){
             //If attackers can win in max 3 moves
-            return 100000;
+            return 100000*(depth+1);
         }
 
         return 0;
